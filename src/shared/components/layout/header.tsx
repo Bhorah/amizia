8e22673651f48067af0c14be8ef52a54abfb5848
Bastien ROY <bastien.roy99@orange.fr>
2025-03-23 13:58:00 +0100

    Logo transition + maquette amizia [skip netlify]
    (cherry picked from commit af4785a5b05608658df039865b45c2cedd7eae76)

--- src/shared/components/layout/header.tsx
+++ src/shared/components/layout/header.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
-import BackGroundScrollTransitionner from '@/shared/components/interaction/backGroundScrollTransitionner'
+import ScrollReactiveHeader from '@/shared/components/interaction/scrollReactiveHeader'
 
 export default function Header() {
 	return (
 		<header>
-			<BackGroundScrollTransitionner>
-				<div className='flex justify-center w-full h-full items-center'>
-					<Image src='/icon.png' alt="Logo d'Amizia" width={40} height={40} />
-					<p className='font-bold text-xl'>Amizia</p>
-				</div>
-			</BackGroundScrollTransitionner>
+			<ScrollReactiveHeader>
+				<></>
+			</ScrollReactiveHeader>
 		</header>
 	)
 }
