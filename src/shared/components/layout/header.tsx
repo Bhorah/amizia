4c2441281a2251161b7f58bed9b7fe3624127da4
Bastien ROY <bastien.roy99@orange.fr>
2025-03-23 14:19:39 +0100

    ESLint
    (cherry picked from commit e176321d2d587b069bb5f2336ad141dd9aac2e51)

--- src/shared/components/layout/header.tsx
+++ src/shared/components/layout/header.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
 import ScrollReactiveHeader from '@/shared/components/interaction/scrollReactiveHeader'
 
 export default function Header() {
