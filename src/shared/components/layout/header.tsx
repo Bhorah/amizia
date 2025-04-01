a5fbd21b0e0358365debffa7096aca13fc3ab1cc
Bhorah <bastien.roy99@orange.fr>
2025-04-01 23:32:47 +0200

    Copy test repo

---
+++ src/shared/components/layout/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ScrollReactiveHeader from '@/shared/components/interaction/scrollReactiveHeader'
+
+export default function Header() {
+	return (
+		<header>
+			<ScrollReactiveHeader>
+				<></>
+			</ScrollReactiveHeader>
+		</header>
+	)
+}
